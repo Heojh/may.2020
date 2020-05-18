bd9f6490d8d7dfd775342cd4e55346ccd9d085c2
heojh <jihaeng1989@gmail.com>
2020-05-18 16:11:35 +0900

    [add] shiritori.js '엔터 키'로 이벤트를 받기 위해  'form'태그 생성.

--- shiritori01/shiritori.js
+++ shiritori01/shiritori.js
@@ -1,20 +1,31 @@
 const body = document.body;
 
+// 첫 단어.
 const word = document.createElement('div');
 word.textContent = '개발자';
 body.append(word);
 
-const inputData = document.createElement('input');
-body.append(inputData);
+// 버튼을 '엔터 키'로 작동 시키기 위해 'form' 생성.
+const form = document.createElement('form');
+body.append(form);
 
+// 입력 창.
+const inputData = document.createElement('input');
+// 'form' 안에 들어가 있어야 한다. body.append -> form.append
+form.append(inputData);
+
+// 버튼(click).
 const button = document.createElement('button');
 button.textContent = '입력';
-body.append(button);
+// 'form' 안에 들어가 있어야 한다. body.append -> form.append
+form.append(button);
 
+// 결과 창.
 const result = document.createElement('div');
 body.append(result);
 
-button.addEventListener('click', function callback() {
+// button -> form / 'click' -> 'submit'
+form.addEventListener('submit', function callback() {
     if(word.textContent[word.textContent.length - 1] === inputData.value[0]) {
         result.textContent = '딩동댕'
         word.textContent = inputData.value;
